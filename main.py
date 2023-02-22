98ddc1cc607195d0bd09bf8cfdb50671de22be91
Jackson Valdez <jacksonvaldez64@gmail.com>
2023-02-22 03:12:14 -0700

    Backprop progress

--- main.py
+++ main.py
@@ -20,18 +20,6 @@ def save_image(pixels):
 # Load the MNIST dataset
 images_test, labels_test = load_mnist('mnist_data', kind='t10k')
 
-pixels = images_test[7623] # Gets the pixel values of the 487th image in the testing set
-save_image(pixels) # Saves the 69th image of the testing set as mnist_digit.png
-
-
-print('-------------- RESULT --------------')
-neural_net = NeuralNetwork()
-query = neural_net.query(pixels)
-number = np.argmax(query[4])
-print(query[4])
-print('This number is a ', number, '!')
 print('-------------- TEST RESULTS --------------')
+neural_net = NeuralNetwork()
 print(neural_net.test_model(images_test, labels_test), 'out of 10,000 correct')
-
-
-
