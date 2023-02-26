98647d0bf63527270a6dc587c0b4130e125e40f8
Jackson Valdez <jacksonvaldez64@gmail.com>
2023-02-26 01:42:55 -0700

    Add exponential decrease for learn rate

--- random_params.py
+++ random_params.py
@@ -8,6 +8,8 @@ weights2 = np.random.uniform(-0.05, 0.05, (10, 16)) # Weights connecting the hid
 biases1 = np.full(16, 0, dtype=np.float64).reshape(16, 1) # Biases connecting the input layer and the hidden layer (16 x 1)
 biases2 = np.full(10, 0, dtype=np.float64).reshape(10, 1) # Biases connecting the hidden layer and the output layer (10 x 1)
 
+epochs = [0]
+
 # print('weights1:', weights1.shape, weights1.dtype)
 # print('weights2:', weights2.shape, weights2.dtype)
 # print('biases1:', biases1.shape, biases1.dtype)
@@ -16,3 +18,4 @@ np.save('trained_params/weights1.npy', weights1)
 np.save('trained_params/weights2.npy', weights2)
 np.save('trained_params/biases1.npy', biases1)
 np.save('trained_params/biases2.npy', biases2)
+np.save('epochs.npy', epochs)
--- train_model.py
+++ train_model.py
@@ -3,19 +3,32 @@ from mnist_data.load import load_mnist
 import numpy as np
 import pdb
 
+def get_learning_rate(epoch):
+    base_lr = 0.1  # Starting learning rate
+    lr = base_lr * (0.1 ** (epoch // 10))  # Decrease learning rate by a factor of 10 every 10 epochs
+    return lr
 
-print('Model Training .....')
+for x in range(500):
+	print('Model Training .....')
 
-images_train, labels_train = load_mnist('mnist_data', kind='train')
-neural_net = NeuralNetwork()
-trained_params = neural_net.train(images_train, labels_train, 0.0000005)
+	images_train, labels_train = load_mnist('mnist_data', kind='train')
+	neural_net = NeuralNetwork()
 
-np.save('trained_params/weights1.npy', trained_params[0])
-np.save('trained_params/weights2.npy', trained_params[1])
-np.save('trained_params/biases1.npy', trained_params[2])
-np.save('trained_params/biases2.npy', trained_params[3])
+	epochs = np.load('epochs.npy')
+	learn_rate = get_learning_rate(epochs[0])
+	print('Epoch:', epochs[0])
+	print('Learning Rate', learn_rate)
 
-print('Model Trained!')
-# print('Computing Cost...')
-# new_cost = neural_net.compute_cost(images_train, labels_train)
-# print('Cost:', new_cost)
+	trained_params = neural_net.train(images_train, labels_train, learn_rate)
+	epochs += 1
+
+	np.save('trained_params/weights1.npy', trained_params[0])
+	np.save('trained_params/weights2.npy', trained_params[1])
+	np.save('trained_params/biases1.npy', trained_params[2])
+	np.save('trained_params/biases2.npy', trained_params[3])
+	np.save('epochs.npy', epochs)
+
+	print('Model Trained!')
+	# print('Computing Cost...')
+	# new_cost = neural_net.compute_cost(images_train, labels_train)
+	# print('Cost:', new_cost)
