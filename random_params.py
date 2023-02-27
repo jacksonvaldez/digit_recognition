877e481965e29549bfc1b39694923242fea2ce71
Jackson Valdez <jacksonvaldez64@gmail.com>
2023-02-27 07:02:53 -0700

    Change HL neuron count to 160

--- random_params.py
+++ random_params.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 # Creates a random set of weights
-weights1 = np.random.uniform(-0.05, 0.05, (128, 784)) # Weights connecting the input layer and the hidden layer (128 x 784)
-weights2 = np.random.uniform(-0.05, 0.05, (10, 128)) # Weights connecting the hidden layer and the output layer (10 x 128)
+weights1 = np.random.uniform(-0.05, 0.05, (160, 784)) # Weights connecting the input layer and the hidden layer (160 x 784)
+weights2 = np.random.uniform(-0.05, 0.05, (10, 160)) # Weights connecting the hidden layer and the output layer (10 x 160)
 
 # Creates a set of biases, all 0 to start
-biases1 = np.full(128, 0, dtype=np.float64).reshape(128, 1) # Biases connecting the input layer and the hidden layer (128 x 1)
+biases1 = np.full(160, 0, dtype=np.float64).reshape(160, 1) # Biases connecting the input layer and the hidden layer (160 x 1)
 biases2 = np.full(10, 0, dtype=np.float64).reshape(10, 1) # Biases connecting the hidden layer and the output layer (10 x 1)
 
 epoch = [0]
