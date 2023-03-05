5fd683ba0cc1a2cc56f3b4e1e213c34cd419e964
Jackson Valdez <jacksonvaldez64@gmail.com>
2023-03-05 01:45:15 -0700

    Train model to 95% testing accuracy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from neural_network import NeuralNetwork
+import pdb
+import cv2
 from mnist_data.load import load_mnist
 
 
@@ -13,17 +15,14 @@ def save_image(pixels):
     plt.savefig('mnist_digit.png')
     return
 
-
-# Load the MNIST dataset
-images_test, labels_test = load_mnist('mnist_data', kind='t10k')
-save_image(images_test[0])
-
-
-print('-------------- TESTING RESULTS --------------')
 weights1 = np.load('trained_params/weights1.npy')
 weights2 = np.load('trained_params/weights2.npy')
 biases1 = np.load('trained_params/biases1.npy')
 biases2 = np.load('trained_params/biases2.npy')
 
 neural_net = NeuralNetwork(weights1, weights2, biases1, biases2)
-print(f"Testing Accuracy: {neural_net.testing_accuracy(images_test, labels_test)}%")
+images_test, labels_test = load_mnist('mnist_data', kind='t10k')
+
+print('---------------------------------------------------------------')
+print(f'Testing Accuracy: {neural_net.testing_accuracy(images_test, labels_test)}%')
+print('---------------------------------------------------------------')
